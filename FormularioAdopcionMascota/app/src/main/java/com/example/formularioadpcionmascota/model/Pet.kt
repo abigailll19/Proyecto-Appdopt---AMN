8cb1440dec2e0493611505f7234fe1a6d748867a
abigailll19 <166961543+abigailll19@users.noreply.github.com>
2026-06-28 11:17:36 -0500

    Eliminar archivos del proyecto raíz y conservar solo Appdopt-MMVM

--- FormularioAdopcionMascota/app/src/main/java/com/example/formularioadpcionmascota/model/Pet.kt
+++
@@ -1,6 +0,0 @@
-package com.example.formularioadpcionmascota.model
-
-data class Pet(
-    val id: Int,
-    val name: String
-)
--- app/src/main/java/com/example/appdopt/model/User.kt
+++
@@ -1,8 +0,0 @@
-package com.example.appdopt.model
-
-data class User(
-    val id: Int = 1,
-    val name: String = "Danna",
-    val email: String = "danna@example.com",
-    val phone: String = "0987654321"
-)
